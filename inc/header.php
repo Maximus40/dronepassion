7c0d56ae912bc9ff1089c871a277cbec9a024d6b
Unknown <m.arbouille@gmail.com>
2018-12-10 16:43:58 +0100

    logo and parallax video

--- inc/header.php
+++ inc/header.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand text-white" href="index.php">Passion Drone</a>
+  <a class="navbar-brand text-white" href="index.php"><img src="img/passion-drone.svg" alt="logo" class="logo"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-include('inc/header.php');
+include('inc/header.php');?>
+
+<header>
+  <div class="overlay"></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+  </video>
+  <div class="container h-100">
+    <div class="d-flex text-center h-100">
+      <div class="my-auto w-100 text-white">
+      </div>
+    </div>
+  </div>
+</header>
