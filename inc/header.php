a778878217bc0f1bae03575a02cade211eadf39a
Unknown <m.arbouille@gmail.com>
2018-12-11 16:24:07 +0100

    change the color of the search bar

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,7 +23,7 @@
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
+      <input class="form-control mr-sm-2 bg-dark" type="search" placeholder="Rechercher" aria-label="Search">
     </form>
       <a href="register.php" class="btn btn-outline-primary my- my-sm-2 mr-1">Inscription</a> 
       <a href="login.php" class="btn btn-outline-primary my- my-sm-2 mr-1">Upload <i class="fas fa-cloud-upload-alt"></i></a> 
