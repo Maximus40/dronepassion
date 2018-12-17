44772a6add29896a5ff37ca609d40e6dd7387b16
Theo Guerinot <theo.guerinot2@gmail.com>
2018-12-17 16:05:36 +0100

    rangement index

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,9 @@
+<?php
+if(session_status()=== PHP_SESSION_NONE){
+  session_start();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,7 +12,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css">
     <link rel="stylesheet" href="css/style.css">
+   
     <title>Drone Passion</title>
 </head>
 <body>
@@ -25,7 +34,33 @@
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2 bg-dark border border-primary" type="search" placeholder="Rechercher" aria-label="Search">
     </form>
+    <?php if(isset($_SESSION['auth'])): ?>
+      <a href="login.php" class="btn btn-outline-primary my- my-sm-2 mr-1">Upload <i class="fas fa-cloud-upload-alt"></i></a> 
+      <a href="">Mon compte</a>
+<?php else: ?>
       <a href="register.php" class="btn btn-outline-primary my- my-sm-2 mr-1">Inscription</a> 
       <a href="login.php" class="btn btn-outline-primary my- my-sm-2 mr-1">Upload <i class="fas fa-cloud-upload-alt"></i></a> 
+<?php endif; ?>
   </div>
 </nav>
+
+
+<div class="container">
+    <?php if(isset($_SESSION['flash'])): ?>
+
+
+    <?php foreach($_SESSION['flash'] as $type => $message): ?>
+
+      <div class="alert alert-<?= $type;?>">
+        <?= $message; ?>
+      </div>
+
+
+    <?php endforeach; ?>
+
+    <?php unset($_SESSION['flash']); ?>
+
+
+    <?php endif; ?>
+
+</div>
--- index.php
+++ index.php
@@ -16,27 +16,8 @@ include('inc/header.php');?>
 
 
  
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css">
 
-<!-- Demo styles -->
-<style>
-    
-     
-    .swiper-container {
-      width: 100%;
-      padding-top: 50px;
-      padding-bottom: 50px;
-      
-    }
-    .swiper-slide {
-      background-position: center;
-      background-size: cover;
-      width: 300px;
-      height: 300px;
-      background:white;
-    }
-  </style>
+
 </head>
 <body>
 <h1 class="text-center mt-2 text-primary">Collections</h1>
@@ -56,29 +37,6 @@ include('inc/header.php');?>
     <!-- Add Pagination -->
     <div class="swiper-pagination"></div>
   </div>
-<!-- Swiper JS -->
-<script src="../dist/js/swiper.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/js/swiper.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/js/swiper.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/js/swiper.esm.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/js/swiper.esm.bundle.js"></script>
 
-<!-- Initialize Swiper -->
-<script>
-    var swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });
-  </script>
+  <?php
+include('inc/footer.php');?>
