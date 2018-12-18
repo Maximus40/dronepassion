f98996d45b8225902e77c7c3e8c7c5ae7891b545
Theo Guerinot <theo.guerinot2@gmail.com>
2018-12-18 13:17:22 +0100

    include index

--- index.php
+++ index.php
@@ -1,42 +1,4 @@
 <?php
-include('inc/header.php');?>
 
-<header>
-  <div class="overlay"></div>
-  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-    <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
-  </video>
-  <div class="container h-100">
-    <div class="d-flex text-center h-100">
-      <div class="my-auto w-100 text-white">
-      </div>
-    </div>
-  </div>
-</header>
-
-
- 
-
-
-</head>
-<body>
-<h1 class="text-center mt-2 text-primary">Collections</h1>
-<div class="swiper-container">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide" >test</div>
-      <div class="swiper-slide" >test</div>
-      <div class="swiper-slide" >test</div>
-      <div class="swiper-slide" >test</div>
-      <div class="swiper-slide" >test</div>
-      <div class="swiper-slide" >test</div>
-      <div class="swiper-slide" ></div>
-      <div class="swiper-slide" ></div>
-      <div class="swiper-slide" ></div>
-      <div class="swiper-slide" ></div>
-    </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
-  </div>
-
-  <?php
-include('inc/footer.php');?>
+include 'accueil.php';
+?>
